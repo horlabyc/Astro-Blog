ef6d0726c4c99551a7826a4fd402ec067eba744f
Olaosebikan Sulaiman <olabisisulaiman@gmail.com>
2023-01-22 21:31:56 +0100

    dynamic page generation for tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel()
+  // output: 'server',
+  // adapter: vercel()
 });
