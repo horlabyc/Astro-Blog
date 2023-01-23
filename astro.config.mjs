5ee647c8de0ca14525daf11b4dc495f4d71fcd94
Olaosebikan Sulaiman <olabisisulaiman@gmail.com>
2023-01-23 20:53:39 +0100

    done

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
 export default defineConfig({
-  // output: 'server',
-  // adapter: vercel()
-});
+  integrations: [preact()]
+} // output: 'server',
+// adapter: vercel()
+);
